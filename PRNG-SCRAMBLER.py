026215abefa2455aa181107f1a5bb872d2adc1c8
TA-True <Thomas.Allen@stu.burgmann.act.edu.au>
2025-05-29 10:30:18 +1000

    Functionality increased
    added more clear instructions
    explained when messages need to be redone
    made the lists more readable
    added a bit more randomness to the first number

--- PRNG-SCRAMBLER.py
+++ PRNG-SCRAMBLER.py
@@ -9,7 +9,7 @@ def rollindecypher(cypher, startshift, encodedmessage):
         i += 1
     encodedmessage = code
     encodedmessage.split
-    fibbogen = [1, 1]
+    fibbogen = [1, 2]
     encoded = []
     i = 0
     while i < msglen:
@@ -40,7 +40,7 @@ def rollincypher(cypher, startshift, message):
     currentshift = cypher.index(startshift)
     cypherlen = len(cypher)
     message.split
-    fibbogen = [1, 1]
+    fibbogen = [1, 2]
     encoded = []
     msglen = len(message)
     nextshift = 0
@@ -88,14 +88,28 @@ def scramblecypher(message, shift):
 cypheraddition = "Null"
 cypher = []
 while cypheraddition != "COMPLETED":
-    if cypheraddition != "Null":
+    if len(cypheraddition) > 2 and cypheraddition != "Null":
+        print("The cypher can only take in single characters")
+    elif cypheraddition == " ":
+        print("The cypher breaks when spaces are used, it is recommended that you use '_' instead.")
+    elif cypheraddition != "Null":
         if cypheraddition not in cypher:
             cypher.append(cypheraddition)
         else:
-            print("Numbers cannot appear twice in the cypher!")
+            print("Characters cannot appear twice in the cypher!")
     print('To stop the cypher creation process submit "COMPLETED" inn all caps to the input question.')
     cypheraddition = input("What is the next letter/symbol you want to put into the cypher: ")
-message = input("What is the desired message to be encoded: ")
+messagework = False
+while messagework == False:
+    message = input("What is the desired message to be encoded: ")
+    messagework = True
+    i = 0
+    while i < len(message):
+        if message[i] not in cypher:
+            messagework = False
+            print("Your message may only contain characters from the cypher.")
+            print("The Cypher is made up of these characters: " + cypher)
+        i += 1
 startshift = "Null"
 while startshift not in cypher:
     startshift = input("What would you like to have the rolling cypher start off with as a shift (choose a part of the cypher): ")
@@ -107,5 +121,15 @@ if scrambled == "Yes":
     horizshift = int(input("What number will you be shifting the message by? "))
     encodedmsg = scramblecypher(encodedmsg, horizshift)
 decodedmsg = rollindecypher(cypher, startshift, encodedmsg)
+i = 0
+encodedstring = ""
+while i < len(encodedmsg):
+    encodedstring += encodedmsg[i]
+    i += 1
 print(encodedmsg)
+decodedstring = ""
+i = 0
+while i < len(decodedmsg):
+    decodedstring += decodedmsg[i]
+    i += 1
 print(decodedmsg)
